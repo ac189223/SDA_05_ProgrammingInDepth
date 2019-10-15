7cf92d137ec7a25af2e75a3ded304ea9fd21ad60
andrzejcalka <ac189223@o2.pl>
2019-10-15 23:56:06 +0200

    SizeMethod

--- src/main/java/exercises/LinkedList.java
+++ src/main/java/exercises/LinkedList.java
@@ -26,21 +26,38 @@ public class LinkedList {
         }
     }
 
-    public int get(int index) {                             // 2 cases
-        if (first == null) {                                // case 1: zero elements
+    public int get(int index) {                                 // 2 cases
+        if (first == null) {                                    // Case 1: zero elements
+                                                                // throw exception
             throw new IndexOutOfBoundsException("list is empty");
-        } else {                                            // case 2: at least one element
-            int counnter = 0;
-            Node current = first;
-            while (current.next != null && counnter < index) {
-                current = current.next;
-                counnter++;
+        } else {                                                // Case 2: at least one element
+            int counnter = 0;                                   // It counts at which element we are
+            Node current = first;                               // Starting from first
+            while (current.next != null && counnter < index) {  // If there exist next one && desired index is not reached
+                current = current.next;                         // Then go to successor
+                counnter++;                                     // Increase counter
             }
-            if (counnter == index) {
-                return current.elem;
+            if (counnter == index) {                            // If we reached desired index
+                return current.elem;                            // Return the element of current node
             } else {
+                                                                // If desired index is not reachable
+                                                                // throw exception
                 throw new IndexOutOfBoundsException("index greater then the lenght of the list");
             }
+        }
+    }
+
+    public int size(LinkedList list) {
+        if (first == null) {                                    // Case 1: zero elements
+            return 0;
+        } else {                                                // Case 2: at least one element
+            int counter = 0;                                    // It counts at which element we are
+            Node current = first;                               // Starting from first
+            while (current.next != null) {                      // If there exist next one
+                current = current.next;                         // Then go to successor
+                counter++;                                      // Increase counter
+            }
+            return counter + 1;                                 // Return the number of elements (counted from 0)
         }
     }
 }
