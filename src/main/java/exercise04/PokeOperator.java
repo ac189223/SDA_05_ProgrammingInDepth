eb83ac8d33e4786704756e3281f0f4b4344e516b
andrzejcalka <ac189223@o2.pl>
2019-10-25 17:47:48 +0200

    Exercise04

--- src/main/java/exercise04/PokeOperator.java
+++ src/main/java/exercise04/PokeOperator.java
@@ -467,4 +467,22 @@ public class PokeOperator {
         return printout.toString();
     }
 
+    private class PokeTypeDataSet {
+        private ArrayList<String> proAttack;
+        private ArrayList<String> poorAttack;
+        private ArrayList<String> proDefence;
+        private ArrayList<String> poorDefence;
+
+        public PokeTypeDataSet() {
+            this.proAttack = new ArrayList<>();
+            this.poorAttack = new ArrayList<>();
+            this.proDefence = new ArrayList<>();
+            this.poorDefence = new ArrayList<>();
+        }
+
+        public ArrayList<String> getProAttack() { return proAttack; }
+        public ArrayList<String> getPoorAttack() { return poorAttack; }
+        public ArrayList<String> getProDefence() { return proDefence; }
+        public ArrayList<String> getPoorDefence() { return poorDefence; }
+    }
 }
